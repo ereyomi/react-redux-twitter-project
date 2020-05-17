410f3b19b53476555724a2908974dc87fad54e7b
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2020-05-17 18:58:26 +0100

    refactor: liking a tweet added to tweet component

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,8 +1,34 @@
+import { saveLikeToggle } from '../utils/api';
+
+export const TOGGLE_TWEET = 'TOGGLE_TWEETS';
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS';
 
 export function receiveTweets (tweets) {
     return {
         type: RECEIVE_TWEETS,
         tweets,
+    }
+}
+
+function toggleTweets ({ id, authedUser, hasLiked }) {
+    return {
+        type: TOGGLE_TWEET,
+        id,
+        authedUser,
+        hasLiked,
+    }
+}
+
+export function handleToggleTweet (info) {
+    return (dispatch) => {
+        dispatch(toggleTweets(info))
+
+        return saveLikeToggle(info)
+            .catch((e) => {
+                console.log('Error in handleToggleTweet: ', e);
+                dispatch(toggleTweets(info));
+                alert('There was an error liking the tweet. Try again.');
+            })
+        
     }
 }
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { formatTweet, formatDate } from '../utils/helpers';
 import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline } from 'react-icons/ti';
-
+import { handleToggleTweet } from '../actions/tweets';
 
 
 class Tweet extends Component {
@@ -10,8 +10,16 @@ class Tweet extends Component {
     e.preventDefault();
     
   }
-  handleLike = () => {
+  handleLike = (e) => {
+    e.preventDefault();
 
+    const { dispatch, tweet, authedUser } = this.props;
+
+    dispatch(handleToggleTweet({
+      id: tweet.id,
+      hasLiked: tweet.hasLiked,
+      authedUser,
+    }))
   }
   render() {
       const { tweet } = this.props;
